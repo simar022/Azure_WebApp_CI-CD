cf05f251db3d9ae6e8b2db05b90123a93948c95c
Simar <simar022netsmartz@gmail.com>
2026-03-19 20:00:10 +0530

    Changes to DB

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 import os
-from pymongo import MongoClient
+from pymongo import MongoClient, errors
 from datetime import datetime
+
 
 # Connection string will be pulled from Azure Environment Variables
 MONGO_URI = os.environ.get("MONGO_URI")
@@ -10,11 +11,15 @@ def get_db():
     return client['cloud_deploy_db']
 
 def add_visit_log():
-    db = get_db()
-    db.logs.insert_one({
-        "timestamp": datetime.now(),
-        "event": "Page Visit"
-    })
+    try:
+        # Set a short timeout (e.g., 3 seconds) so the user doesn't wait 30s for a 500 error
+        client = MongoClient(os.environ.get("MONGO_URI"), serverSelectionTimeoutMS=3000)
+        db = client['mongodb022']
+        db.logs.insert_one({"timestamp": datetime.now(), "event": "Page Visit"})
+    except errors.ServerSelectionTimeoutError as e:
+        print(f"Database connection timed out: {e}")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
 
 def get_logs():
     db = get_db()
